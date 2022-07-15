eeba17dd4297df522985b29e1d64cdfb05b0f87b
BMills <105277453+BillyMillsGitHub@users.noreply.github.com>
2022-07-15 20:57:06 +0100

    Uploading Back-end data to Github Repository

---
+++ Week-9-Hobby-Project-API/src/main/java/com/example/project/rest/CharacterCreatorController.java
@@ -0,0 +1,77 @@
+package com.example.project.rest;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project.entity.CharacterCreator;
+import com.example.project.service.CharacterCreatorService;
+
+@CrossOrigin
+@RestController
+public class CharacterCreatorController {
+
+
+
+	@Autowired
+	private CharacterCreatorService service; // dependency
+
+	@GetMapping("/helloCharacterCreator")
+	public String helloWorld() {
+		String a = "This is a controller test about creating characters for a game! ";
+		String b = "This is a second paragraph";
+		return a + b;
+	}
+
+	@GetMapping("/demoCharacterCreator")
+	public CharacterCreator getDemoCharacterCreator() {
+		return new CharacterCreator("Magman", "Fireball", 5000, 950, 1100, 850, 1500, 2100);
+	}
+
+	@GetMapping("/getCharacterCreator")
+	public ResponseEntity<List<CharacterCreator>> getAll() {
+		return new ResponseEntity<List<CharacterCreator>>(this.service.getAll(), HttpStatus.OK);
+	}
+
+	@GetMapping("/getCharacterCreatorById/{id}")
+	public CharacterCreator getById(@PathVariable Integer id) {
+		return this.service.getById(id);
+	}
+
+	@PostMapping("/createCharacterCreator")
+	public ResponseEntity<CharacterCreator> create(@RequestBody CharacterCreator characterCreator) {
+
+		System.out.println("Created: " + characterCreator);
+		CharacterCreator created = this.service.create(characterCreator);
+		return new ResponseEntity<CharacterCreator>(created, HttpStatus.CREATED);
+	}
+
+	@PatchMapping("/updateCharacterCreator/{id}")
+	public CharacterCreator update(@PathVariable Integer id, @PathParam("name") String name, @PathParam("specialAbility") String specialAbility,
+			@PathParam("power") Integer power, @PathParam("health") Integer health, @PathParam("attack") Integer attack, @PathParam("defence") Integer defence, 
+			@PathParam("agility") Integer agility, @PathParam("intelligence") Integer intelligence){
+		return this.service.update(id, name, specialAbility, power, health, attack, defence, agility, intelligence );
+
+	}
+
+	// id = 1
+
+	@DeleteMapping("/removeCharacterCreator/{id}")
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		System.out.println("ID: " + id);
+		this.service.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
---
+++ Week-9-Hobby-Project-API/target/classes/com/example/project/CharacterCreator/CharacterCreator_Schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `characterCreator` CASCADE;
+create table `characterCreator` (
+	`id` INTEGER UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+    `specialAbility` VARCHAR(255),
+    `health` INTEGER NOT NULL,
+    `attack` INTEGER NOT NULL,
+    `defence` INTEGER NOT NULL,
+    `agility` INTEGER NOT NULL,
+    `intelligence` INTEGER NOT NULL
+);
