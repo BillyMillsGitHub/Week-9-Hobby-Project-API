97bb4f24cce3bf0131b3a132b9085c65a6eb8b8c
Billy Mills <bmills94@outlook.com>
2022-07-29 12:08:01 +0100

    modified

---
+++ target/classes/CharacterCreator_Data.sql
@@ -0,0 +1,22 @@
+INSERT INTO `character_Creator` (
+	`id`,
+	`name`,
+	`special_Ability`,
+	`power`,
+	`health`,
+    `attack`,
+    `defence`,
+    `agility`,
+    `intelligence`
+) 
+VALUES (
+	default,
+	'Magman',
+	'Fireball',
+    5000,
+    950,
+    1100,
+    850,
+    1500,
+    2100
+);
